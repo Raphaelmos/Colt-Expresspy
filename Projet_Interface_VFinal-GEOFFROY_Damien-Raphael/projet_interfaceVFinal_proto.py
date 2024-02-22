7af9d662087536b3216e2ae5487f5f055c727a60
Raph <96924364+Raphaelmos@users.noreply.github.com>
2024-02-22 16:29:26 +0100

    Update projet_interfaceVFinal_proto.py

--- Projet_Interface_VFinal-GEOFFROY_Damien-Raphael/projet_interfaceVFinal_proto.py
+++ Projet_Interface_VFinal-GEOFFROY_Damien-Raphael/projet_interfaceVFinal_proto.py
@@ -17,6 +17,7 @@ from bandit import *
 # game.title("rd")
 # game.geometry("800x600")
 # Code explained in every part and can also have a english version in the future 
+# Problems can appear if everything isn't installed for example the music and mp3 format can witness issues 
 class Jeu(tk.Tk):
     def __init__(self):    
         
